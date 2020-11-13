61624aef3cda74939c3abb3c4a45c4a6e3d99f1a
Barri <info@bdesignstudio.co.uk>
2020-11-13 11:30:37 +0000

    Adjust the code for new game

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 /******************* Boilerplate **************************/
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+const {
+  Engine,
+  Render,
+  Runner,
+  World,
+  Bodies
+} = Matter;
+const width = 800;
+const height = 600;
 
-//**1- new engine
 const engine = Engine.create();
 //**2-access snapshots of the object we have 
 const { world } = engine;
-//---------------------------------
-const width = 800;
-const height = 600;
-//---------------------------------
 //**-3 render them on screen
 const render = Render.create({
   //** 4- telling render where to show the shapes in our html doc----------------
@@ -23,42 +26,22 @@ const render = Render.create({
   }
 });
 
-
 //7- Using Render to start rendering our render object
 Render.run(render);
 // 8- run everything with code below
 Runner.run(Runner.create(), engine);
-World.add(world, MouseConstraint.create(engine, {
-  mouse: Mouse.create(render.canvas)
-}));
-/********************* sample to make a shape ************************/
-
-// 9- create a shape -- from top left of the screen to the center of the object x, y axis:200,  width and height: 50
-//const shape = Bodies.rectangle(200, 200, 50, 50, {
-//isStatic keeps the shape in place at start
-//isStatic: true
-//});
-//10- Drop it to world
-//World.add(world, shape)
 
 /******** Making a Demo *********/
 //walls
 const walls = [
-  Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
-  Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
-  Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
-  Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
 ]
 World.add(world, walls);
 // Single shape
 World.add(world, Bodies.rectangle(200, 200, 50, 50));
-// Random Shapes place and sizes
-for (let i = 0; i < 40; i++) {
-  if (Math.random() > 0.5) {
-    World.add(world, Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50));
-  } else {
-    World.add(world, Bodies.circle(Math.random() * width, Math.random() * height, 35 ));
-  }
-}
+
 
 
